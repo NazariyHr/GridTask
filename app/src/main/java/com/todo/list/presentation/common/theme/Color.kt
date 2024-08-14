9fcadf5b29b0d999605d51726441aa3ccef874a4
Nazar <nazariu11@gmail.com>
2024-08-14 13:08:01 +0300

    set up start dependencies, base project packages structure and root theme

---
+++ app/src/main/java/com/todo/list/presentation/common/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.todo.list.presentation.common.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val MainBgColor = Color(82, 192, 206)
